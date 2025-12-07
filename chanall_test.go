65919823d0d5892b5034f920cb7df3af2cec4d75
Oleg Subachev <git@solsw.ru>
2025-12-07 21:56:10 +0500

    ChanAll functions moved here from go2linq package

---
+++ chanall_test.go
@@ -0,0 +1,83 @@
+package iterhelper
+
+import (
+	"iter"
+	"testing"
+)
+
+func closedCh() chan int {
+	var ch = make(chan int)
+	close(ch)
+	return ch
+}
+
+func chn2() chan int {
+	var ch = make(chan int)
+	go func() {
+		ch <- 1
+		close(ch)
+	}()
+	return ch
+}
+
+func chn3() chan int {
+	ch := make(chan int)
+	go func() {
+		ch <- 4
+		ch <- 3
+		ch <- 2
+		ch <- 1
+		close(ch)
+	}()
+	return ch
+}
+
+func TestChanAll_int(t *testing.T) {
+	type args struct {
+		c <-chan int
+	}
+	tests := []struct {
+		name string
+		args args
+		want iter.Seq[int]
+	}{
+		{name: "nil channel",
+			args: args{c: nil},
+			want: Empty[int](),
+		},
+		{name: "closed channel",
+			args: args{c: closedCh()},
+			want: Empty[int](),
+		},
+		{name: "2",
+			args: args{c: chn2()},
+			want: VarSeq(1),
+		},
+		{name: "3",
+			args: args{c: chn3()},
+			want: VarSeq(4, 3, 2, 1),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ChanAll(tt.args.c)
+			equal, _ := SeqEqual(got, tt.want)
+			if !equal {
+				t.Errorf("ChanAll() = %v, want %v", StringDef(got), StringDef(tt.want))
+			}
+		})
+	}
+}
+
+func TestChanAll_int_2(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		next, stop := iter.Pull(ChanAll(chn2()))
+		defer stop()
+		_, _ = next()
+		got, _ := next()
+		want := 0
+		if got != want {
+			t.Errorf("ChanAll() = %v, want %v", got, want)
+		}
+	})
+}
